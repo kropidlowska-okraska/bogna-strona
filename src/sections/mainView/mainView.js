0d030ede64f3089e59d87c2d0dd68e862c299a50
okraska-kropidlowski <wojtek.1986@gmail.com>
2023-01-10 23:50:55 +0100

    Create first draft of the layout

--- src/sections/mainView/mainView.js
+++ src/sections/mainView/mainView.js
@@ -1,9 +1,17 @@
+import "./mainView.css";
+import image from "../../assets/logo.png";
+
 const MainView = () => {
   return (
-    <div>
-      <div>Logo</div>
-      <div>Tytul</div>
-      <div>Podtytul</div>
+    <div className="container">
+      <div className="logoContainer">
+        <div className="logo">
+          {" "}
+          <img src={image} alt="" />{" "}
+        </div>
+      </div>
+      <div className="title">Śmieszne Wesołe Ptaszki</div>
+      <div className="subtitle">Sikorka odlatuje z budki lęgowej.</div>
     </div>
   );
 };
