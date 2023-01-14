cb3925e2c6559fcf2b04f90bc8417bada74782de
okraska-kropidlowski <wojtek.1986@gmail.com>
2023-01-14 17:10:47 +0100

    Add custom fonts and more details of styling

--- src/sections/mainView/mainView.js
+++ src/sections/mainView/mainView.js
@@ -17,28 +17,10 @@ const MainView = () => {
         <div>D I</div>
         <div>DOGGOIPSUM</div>
       </nav>
-      <div id="subtitle" className="subtitle">
-        Doggo ipsum big ol pupperino.
+      <div id="title" className="title">
+        DOGGO IPSUM
       </div>
-      <div id="subsubtitle" className="subsubtitle">
-        <p>
-          Doggo ipsum big ol pupperino very taste wow you are doing me a
-          frighten noodle horse, wow such tempt shoob. Ruff corgo doggorino
-          floofs borking doggo, long doggo borking doggo shoob. Heckin good boys
-          and girls heck you are doing me a frighten, big ol pupper. Shooberino
-          fat boi shoober wow very biscit waggy wags big ol, length boy blop vvv
-          you are doing me the shock very taste wow, vvv woofer vvv noodle
-          horse. heckin angery woofer porgo smol borking doggo with a long snoot
-          for pats. Snoot dat tungg tho shibe mlem long woofer vvv, bork aqua
-          doggo pupperino dat tungg tho. Blop long woofer shoob most angery
-          pupper I have ever seen long woofer, most angery pupper I have ever
-          seen vvv sub woofer. Noodle horse smol borking doggo with a long snoot
-          for pats extremely cuuuuuute puggo pats, big ol length boy. Waggy wags
-          what a nice floof shoober porgo floofs shoob, ruff very taste wow
-          pupperino clouds. You are doing me a frighten wow very biscit blop
-          very good spot woofer extremely cuuuuuute ur givin me a spook, long
-          woofer doing me a frighten super chub pupperino long water shoob.
-        </p>
+      <div id="subtitle" className="subtitle">
         <p>
           Stop it fren fluffer borking doggo tungg sub woofer blep, you are
           doing me the shock yapper wrinkler borkdrive big ol, blep tungg doge
@@ -49,22 +31,6 @@ const MainView = () => {
           shooberino doggo I am bekom fat. Long woofer maximum borkdrive clouds
           adorable doggo puggorino, mlem pupper ur givin me a spook. Long water
           shoob he made many woofs heck, aqua doggo.
-        </p>
-        <p>
-          Shoob snoot shibe shooberino aqua doggo corgo, doge noodle horse
-          corgo. Mlem blep thicc doing me a frighten heckin, thicc many pats
-          floofs much ruin diet, stop it fren blep doge. Heck pupper he made
-          many woofs doing me a frighten, doggorino very hand that feed shibe
-          smol very taste wow, borkdrive smol borking doggo with a long snoot
-          for pats. Heckin long water shoob you are doing me the shock, long
-          water shoob. Long doggo borkf long doggo very good spot, smol borking
-          doggo with a long snoot for pats pats. Big ol pupper very good spot
-          tungg, much ruin diet. Thicc I am bekom fat stop it fren heckin angery
-          woofer length boy, borkdrive snoot. Waggy wags big ol pupper porgo I
-          am bekom fat, what a nice floof puggorino. Waggy wags doggorino
-          shooberino long water shoob heckin good boys corgo, wrinkler pupper he
-          made many woofs you are doing me a frighten. Big ol pupper doggo
-          clouds borkdrive, noodle horse maximum borkdrive.
         </p>
       </div>
     </div>
