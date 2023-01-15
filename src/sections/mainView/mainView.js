b8ba9dc18121548018b127b68db4ede84325bc50
marlena.kropidlowska <marlena.kropidlowska@erasys.de>
2023-01-15 19:13:17 +0100

    Adjust main section

--- src/sections/mainView/mainView.js
+++ src/sections/mainView/mainView.js
@@ -3,35 +3,28 @@ import image from "../../assets/logo.png";
 
 const MainView = () => {
   return (
-    <div className="container">
-      <div className="logoContainer">
-        <div className="logo">
-          {" "}
-          <img src={image} />{" "}
-        </div>
+    <div className="mainView__container">
+      <div className="logo">
+        <img src={image} alt={"Logo"} />
       </div>
-      <nav class="navigation">
+      <nav className="navigation">
         <div>DOGGO</div>
         <div>IPSUM</div>
         <div>DOGGO IPSUM</div>
         <div>D I</div>
         <div>DOGGOIPSUM</div>
       </nav>
-      <div id="title" className="title">
-        DOGGO IPSUM
-      </div>
-      <div id="subtitle" className="subtitle">
-        <p>
-          Stop it fren fluffer borking doggo tungg sub woofer blep, you are
-          doing me the shock yapper wrinkler borkdrive big ol, blep tungg doge
-          clouds. Bork doing me a frighten smol borking doggo with a long snoot
-          for pats puggo, blep bork. Borking doggo h*ck heckin good boys and
-          girls borkdrive, extremely cuuuuuute. Sub woofer noodle horse corgo
-          tungg very hand that feed shibe very taste wow, what a nice floof
-          shooberino doggo I am bekom fat. Long woofer maximum borkdrive clouds
-          adorable doggo puggorino, mlem pupper ur givin me a spook. Long water
-          shoob he made many woofs heck, aqua doggo.
-        </p>
+      <div className="mainView__content">
+        <h1 className="title">DOGGO IPSUM</h1>
+        <div className="subtitle">
+          <p>
+            Stop it fren fluffer borking doggo tungg sub woofer blep, you are
+            doing me the shock yapper wrinkler borkdrive big ol, blep tungg doge
+            clouds. Bork doing me a frighten smol borking doggo with a long
+            snoot for pats puggo, blep bork. Borking doggo h*ck heckin good boys
+            and girls borkdrive, extremely cuuuuuute.
+          </p>
+        </div>
       </div>
     </div>
   );
