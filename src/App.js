457922e01091feb12ea93f2aaf014b7b62fbda74
okraska-kropidlowski <wojtek.1986@gmail.com>
2023-01-31 22:49:33 +0100

    Add styling to slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import "./App.css";
 import MainView from "./sections/mainView/MainView";
 import Slider from "react-slick";
+import styles from "./App.module.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import image from "./assets/logo.png";
+import logo from "./assets/logo.png";
+import slider_1 from "./assets/slider_1.jpeg";
+import slider_2 from "./assets/slider_2.jpeg";
+import slider_3 from "./assets/slider_3.jpeg";
 
 // TODO: Check with JavaScript if viewport is mobile or desktop
 const isMobile = false;
@@ -18,38 +21,19 @@ const settings = {
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <MainView />
       {/*TODO: Move to separate component (in src/sections/Gallery ) and import here  */}
       <Slider {...settings}>
-        <div>
-          <h3>
-            <img src={image} alt="" />
-          </h3>
-        </div>
-        <div>
-          <h3>
-            2
-            <iframe
-              width="420"
-              height="315"
-              src="https://www.youtube.com/embed/tgbNymZ7vqY"
-              title={"Muppets video"}
-            ></iframe>
-          </h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        <img src={logo} alt="" className={styles.slider} />
+        <iframe
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+          title={"Muppets video"}
+          className={styles.slider}
+        ></iframe>
+        <img src={slider_1} alt="" className={styles.slider} />
+        <img src={slider_2} alt="" className={styles.slider} />
+        <img src={slider_3} alt="" className={styles.slider} />
       </Slider>
     </div>
   );
