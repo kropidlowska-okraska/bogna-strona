879fd8c30085c914cf287958b1ba11dc5e1c882f
marlena.kropidlowska <marlena.kropidlowska@erasys.de>
2023-03-12 18:06:04 +0100

    Rewrite MainView to use tailwind

--- src/sections/mainView/MainView.js
+++ src/sections/mainView/MainView.js
@@ -1,32 +1,23 @@
-import styles from "./MainView.module.css";
-import image from "../../assets/logo.png";
+import Navigation from "../navigation/Navigation";
+import Heading from "../../components/Heading";
 
 const MainView = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        <img src={image} alt={"Logo"} />
-      </div>
-      <nav className={styles.navigation}>
-        <div>DOGGO</div>
-        <div>IPSUM</div>
-        <div>DOGGO IPSUM</div>
-        <div>D I</div>
-        <div>DOGGOIPSUM</div>
-      </nav>
-      <div className={styles.content}>
-        <h1 className={styles.title}>DOGGO IPSUM</h1>
-        <div className={styles.subtitle}>
-          <p>
+    <>
+      <div className='bg-[url("/src/assets/background_sm.jpeg")] md:bg-[url("/src/assets/mainBackgroundImage.jpg")] bg-cover bg-center bg-fixed h-screen flex flex-col justify-center relative'>
+        <Navigation />
+        <div className="p-4 lg:w-6/12 lg:p-20">
+          <Heading>DOGGO IPSUM TITLE</Heading>
+          <div className="font-lato-regular text-xl text-green-dark lg:text-2xl">
             Stop it fren fluffer borking doggo tungg sub woofer blep, you are
             doing me the shock yapper wrinkler borkdrive big ol, blep tungg doge
             clouds. Bork doing me a frighten smol borking doggo with a long
             snoot for pats puggo, blep bork. Borking doggo h*ck heckin good boys
             and girls borkdrive, extremely cuuuuuute.
-          </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
